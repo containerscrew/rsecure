8b65f4ca30faa69211d137abfeb869ee43788562
containerscrew <containerscrew@users.noreply.github.com>
2025-10-03 21:35:52 +0200

    refactor: log in create-key command

--- src/commands/create_key.rs
+++ src/commands/create_key.rs
@@ -1,7 +1,8 @@
 use crate::cli::CreateKeyArgs;
-use crate::write_to_file;
+use crate::{print_message, write_to_file};
 use aes_gcm::aead::OsRng;
 use aes_gcm::{Aes256Gcm, KeyInit};
+use colored::Colorize;
 
 pub fn run(crt_args: CreateKeyArgs) -> anyhow::Result<()> {
     let key = Aes256Gcm::generate_key(OsRng);
@@ -9,8 +10,8 @@ pub fn run(crt_args: CreateKeyArgs) -> anyhow::Result<()> {
     // Save the key to the specified output file
     write_to_file(&crt_args.output, &[&key.as_slice()])?;
 
-    println!("AES key generated and saved to {}", crt_args.output);
-    println!(
+    print_message!("AES key generated and saved to {}", crt_args.output);
+    print_message!(
         "Please, save this key securely, if you lose it, you won't be able to decrypt your files!"
     );
     Ok(())

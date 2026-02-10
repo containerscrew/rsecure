969f3b0b9e8b3da9a3c103583591cf826611bd3c
containerscrew <containerscrew@users.noreply.github.com>
2026-02-11 00:19:08 +0100

    build: preparing automatic aur package build

--- packaging/arch/build.sh
+++ packaging/arch/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 # Define version
-VERSION="0.3.0"
+VERSION=$1
 TEMPLATE="PKGBUILD.template"
 OUTPUT="PKGBUILD"
 AUR_REPO="ssh://aur@aur.archlinux.org/rsecure.git"
 TEMP_DIR="/tmp/rsecure_build"
+
+if [[ -z "$VERSION" ]]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# Check latest tag is the same as version
+LATEST_TAG=$(git describe --tags --abbrev=0)
+if [[ "$LATEST_TAG" != "$VERSION" ]]; then
+    echo "Error: Latest git tag ($LATEST_TAG) does not match version ($VERSION)"
+    exit 1
+fi
 
 # Fetch source and calculate hash without saving file
 echo "Fetching source to calculate checksum..."
@@ -17,7 +29,7 @@ sed -e "s|@version@|$VERSION|g" \
     "$TEMPLATE" > "$OUTPUT"
 
 # Validate PKGBUILD
-namcap "$OUTPUT"
+# namcap "$OUTPUT"
 
 # Generate .SRCINFO
 echo "Generating .SRCINFO..."
@@ -33,6 +45,8 @@ cd "$TEMP_DIR" || exit 1 # Exit if directory missing
 
 # Only commit and push if there are changes
 if [[ -n $(git status --porcelain) ]]; then
+    git config user.name "containerscrew"
+    git config user.email "info@containerscrew.com"
     git add PKGBUILD .SRCINFO
     git commit -m "Update to v$VERSION"
     git push origin master
@@ -40,6 +54,6 @@ else
     echo "No changes detected, skipping push."
 fi
 
-# Cleanup
-rm -rf "$TEMP_DIR"
-echo "Done! v$VERSION deployed."
+# # Cleanup
+# rm -rf "$TEMP_DIR"
+# echo "Done! v$VERSION deployed."
